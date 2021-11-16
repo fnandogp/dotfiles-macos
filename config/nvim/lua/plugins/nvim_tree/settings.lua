3b10ab2957fecc2fe5580c3e1835cecdae06d8bb
Fernando Pinheiro <fnandogp@gmail.com>
2021-11-17 10:04:45 +1100

    minux nvim tree update

--- config/nvim/lua/plugins/nvim_tree/settings.lua
+++ config/nvim/lua/plugins/nvim_tree/settings.lua
@@ -1,24 +1,13 @@
-vim.g.nvim_tree_show_icons = { git = 0, folders = 1, files = 1 }
-
 vim.g.nvim_tree_git_hl = 0
 vim.g.nvim_tree_quit_on_open = 1
 vim.g.nvim_tree_highlight_opened_files = 1
 vim.g.nvim_tree_group_empty = 1
+vim.g.nvim_tree_window_picker_exclude = {
+	filetype = { "notify", "packer", "qf" },
+	buftype = { "terminal" },
+}
+vim.g.nvim_tree_show_icons = { git = 0, folders = 1, files = 1, folder_arrows = 1 }
 
 require("nvim-tree").setup({
-	open_on_setup = false,
-	open_on_tab = false,
-	update_cwd = false,
-	auto_close = false,
-	follow = 1,
-	update_focused_file = {
-		enable = true,
-		update_cwd = false,
-		ignore_list = {},
-	},
-	show_icons = { git = false, folders = true, files = true },
-	view = {
-		width = 20,
-		height = 20,
-	},
+	update_focused_file = { enable = true },
 })
