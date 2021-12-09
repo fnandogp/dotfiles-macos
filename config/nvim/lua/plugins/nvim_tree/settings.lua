bff6f1d57aecbeaabe987201c58964c14466c33e
Fernando Pinheiro <fnandogp@gmail.com>
2021-12-09 13:23:00 +1100

    .

--- config/nvim/lua/plugins/nvim_tree/settings.lua
+++ config/nvim/lua/plugins/nvim_tree/settings.lua
@@ -3,8 +3,8 @@ vim.g.nvim_tree_quit_on_open = 1
 vim.g.nvim_tree_highlight_opened_files = 1
 vim.g.nvim_tree_group_empty = 1
 vim.g.nvim_tree_window_picker_exclude = {
-	filetype = { "notify", "packer", "qf" },
-	buftype = { "terminal", "fugitive" },
+	filetype = { "notify", "packer", "qf", "fugitive" },
+	buftype = { "terminal" },
 }
 vim.g.nvim_tree_show_icons = {
 	git = 0,
