bb2c2013ac61e0d1e100c122e74b6df57d17901f
Fernando Pinheiro <fnandogp@gmail.com>
2022-10-16 22:56:33 +1100

    lsp mappings

--- config/nvim/lua/plugins/lsp/mappings.lua
+++ config/nvim/lua/plugins/lsp/mappings.lua
@@ -21,14 +21,16 @@ function M.set_default(client, bufnr)
   buf_set_keymap("n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", opts)
   buf_set_keymap("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
   buf_set_keymap("n", "<C-k>", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
-  buf_set_keymap("n", "gl", "<cmd>lua vim.diagnostic.open_float()<CR>", opts)
-  buf_set_keymap("n", "gL", "<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>", opts)
   buf_set_keymap("n", "gn", "<cmd>lua vim.lsp.buf.rename()<CR>", opts)
   buf_set_keymap("n", "gx", "<cmd>lua vim.lsp.buf.code_action()<CR>", opts)
   buf_set_keymap("v", "gx", "<cmd>lua vim.lsp.buf.range_code_action()<CR>", opts)
-  buf_set_keymap("n", "gk", "<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>", opts)
-  buf_set_keymap("n", "gj", "<cmd>lua vim.lsp.diagnostic.goto_next()<CR>", opts)
-  buf_set_keymap("n", "<leader>F", "<cmd>lua vim.lsp.buf.formatting()<CR>", opts)
+
+  buf_set_keymap("n", "gl", "<cmd>lua vim.diagnostic.open_float()<CR>", opts)
+  buf_set_keymap("n", "gL", "<cmd>lua vim.diagnostic.setqflist()<CR>", opts)
+  buf_set_keymap("n", "gk", "<cmd>lua vim.diagnostic.goto_prev()<CR>", opts)
+  buf_set_keymap("n", "gj", "<cmd>lua vim.diagnostic.goto_next()<CR>", opts)
+
+  buf_set_keymap("n", "<leader>F", "<cmd>lua vim.lsp.buf.format({ bufnr = bufnr })<CR>", opts)
 end
 
 return M
