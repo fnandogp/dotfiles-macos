39e079a8b95a3f73e9439e36ddd48d430e9b82b4
Fernando Pinheiro <fnandogp@gmail.com>
2023-10-24 09:37:43 +1100

    wip

--- config/nvim/lua/plugins/init.lua
+++ config/nvim/lua/plugins/init.lua
@@ -15,4 +15,25 @@ return {
     "echasnovski/mini.starter",
     opts = {},
   },
+  {
+    --"echasnovski/mini.basics",
+    --opts = {
+    --options = {
+    --basic = true,
+    --extra_ui = true,
+    --win_borders = "single",
+    --},
+    --mappings = {
+    --basic = true,
+    --option_toggle_prefix = [[\]],
+    --windows = true,
+    --move_with_alt = true,
+    --},
+    --autocommands = {
+    --basic = true,
+    --relnum_in_visual_mode = true,
+    --},
+    --silent = true,
+    --},
+  },
 }
