578f12a5093c81380c26cbe6b63ce03b9720b243
Fernando Pinheiro <fnandogp@gmail.com>
2024-07-29 11:06:30 +1000

    replace spectre with grugfar

---
+++ config/nvim/lua/plugins/editor.lua
@@ -0,0 +1,10 @@
+return {
+  "MagicDuck/grug-far.nvim",
+  cmd = "GrugFar",
+  config = function()
+    require("grug-far").setup({})
+  end,
+  keys = {
+    { "<leader>ss", "<cmd>GrugFar<CR>", desc = "Open GrugFar", mode = "n" },
+  },
+}
--- config/nvim/lua/plugins/spectre.lua
+++ config/nvim/lua/plugins/spectre.lua
@@ -1,13 +1,14 @@
-return {
-  "nvim-pack/nvim-spectre",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-  },
-  opts = {},
-  keys = {
-    { "<leader>sss", '<cmd>lua require("spectre").toggle()<CR>', desc = "Toggle Spectre", mode = "n" },
-    { "<leader>ssw", '<cmd>lua require("spectre").open_visual({select_word=true})<CR>', desc = "Search current word", mode = "n" },
-    { "<leader>ssw", '<esc><cmd>lua require("spectre").open_visual()<CR>', desc = "Search current word", mode = "v" },
-    { "<leader>ssp", '<cmd>lua require("spectre").open_file_search({select_word=true})<CR>', desc = "Search on current file", mode = "n" },
-  },
-}
+return {}
+--return {
+--"nvim-pack/nvim-spectre",
+--dependencies = {
+--"nvim-lua/plenary.nvim",
+--},
+--opts = {},
+--keys = {
+--{ "<leader>sss", '<cmd>lua require("spectre").toggle()<CR>', desc = "Toggle Spectre", mode = "n" },
+--{ "<leader>ssw", '<cmd>lua require("spectre").open_visual({select_word=true})<CR>', desc = "Search current word", mode = "n" },
+--{ "<leader>ssw", '<esc><cmd>lua require("spectre").open_visual()<CR>', desc = "Search current word", mode = "v" },
+--{ "<leader>ssp", '<cmd>lua require("spectre").open_file_search({select_word=true})<CR>', desc = "Search on current file", mode = "n" },
+--},
+--}
