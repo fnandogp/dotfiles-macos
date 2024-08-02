163c0b0bc90fe3f67b0dd9464f545c295006bac8
Fernando Pinheiro <fnandogp@gmail.com>
2024-08-02 12:55:18 +1000

    use startship instead of powerlevel10k

--- config/nvim/lua/plugins/coding.lua
+++ config/nvim/lua/plugins/coding.lua
@@ -1,22 +1,37 @@
 return {
   { "preservim/nerdcommenter" },
-  {
-    "zbirenbaum/copilot.lua",
-    cmd = "Copilot",
-    event = "InsertEnter",
-    config = function()
-      require("copilot").setup({
-        suggestion = {
-          enabled = true,
-          auto_trigger = true,
-          keymap = { accept = "<M-y>" },
-        },
-      })
-    end,
-  },
   { "echasnovski/mini.pairs", opts = {} },
   { "echasnovski/mini.surround", opts = {} },
   { "echasnovski/mini.bufremove", version = false, opts = {} },
   { "echasnovski/mini.move", version = false, opts = {} },
   { "tpope/vim-fugitive" },
+  --{
+  --"zbirenbaum/copilot.lua",
+  --cmd = "Copilot",
+  --event = "InsertEnter",
+  --config = function()
+  --require("copilot").setup({
+  --suggestion = {
+  --enabled = true,
+  --auto_trigger = true,
+  --keymap = { accept = "<M-y>" },
+  --},
+  --})
+  --end,
+  --},
+  {
+    "codota/tabnine-nvim",
+    build = "./dl_binaries.sh",
+    config = function()
+      require("tabnine").setup({
+        disable_auto_comment = true,
+        accept_keymap = "<M-y>",
+        dismiss_keymap = "<M-n>",
+        debounce_ms = 800,
+        exclude_filetypes = { "TelescopePrompt", "NvimTree" },
+        log_file_path = nil, -- absolute path to Tabnine log file
+        ignore_certificate_errors = false,
+      })
+    end,
+  },
 }
