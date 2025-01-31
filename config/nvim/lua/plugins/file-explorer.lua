815478ad4030ffac88d037365c9a6c59a5c25795
Fernando Pinheiro <fernando@convincely.com>
2025-01-31 11:22:52 +1100

    add mini files new keymaps

--- config/nvim/lua/plugins/file-explorer.lua
+++ config/nvim/lua/plugins/file-explorer.lua
@@ -20,7 +20,6 @@ return {
       MiniFiles.refresh({ content = { filter = new_filter } })
     end
 
-    --Create mappings to modify target window via split ~
     vim.api.nvim_create_autocmd("User", {
       pattern = "MiniFilesBufferCreate",
       callback = function(args)
@@ -29,45 +28,68 @@ return {
         vim.keymap.set("n", "g.", toggle_dotfiles, { buffer = buf_id })
       end,
     })
+
+    -- Create mappings to modify target window via split ~
     local map_split = function(buf_id, lhs, direction)
       local rhs = function()
         -- Make new window and set it as target
-        local new_target_window
-        vim.api.nvim_win_call(MiniFiles.get_target_window(), function()
+        local cur_target = MiniFiles.get_explorer_state().target_window
+        local new_target = vim.api.nvim_win_call(cur_target, function()
           vim.cmd(direction .. " split")
-          new_target_window = vim.api.nvim_get_current_win()
+          return vim.api.nvim_get_current_win()
         end)
 
-        MiniFiles.set_target_window(new_target_window)
+        MiniFiles.set_target_window(new_target)
+
+        -- This intentionally doesn't act on file under cursor in favor of
+        -- explicit "go in" action (`l` / `L`). To immediately open file,
+        -- add appropriate `MiniFiles.go_in()` call instead of this comment.
       end
+
       -- Adding `desc` will result into `show_help` entries
       local desc = "Split " .. direction
       vim.keymap.set("n", lhs, rhs, { buffer = buf_id, desc = desc })
     end
+
     vim.api.nvim_create_autocmd("User", {
       pattern = "MiniFilesBufferCreate",
       callback = function(args)
         local buf_id = args.data.buf_id
         -- Tweak keys to your liking
-        map_split(buf_id, "<C-h>", "belowright horizontal")
+        map_split(buf_id, "<C-s>", "belowright horizontal")
         map_split(buf_id, "<C-v>", "belowright vertical")
       end,
     })
 
-    --Create mapping to set current working directory ~
-    local files_set_cwd = function(path)
-      -- Works only if cursor is on the valid file system entry
-      local cur_entry_path = MiniFiles.get_fs_entry().path
-      local cur_directory = vim.fs.dirname(cur_entry_path)
-      vim.fn.chdir(cur_directory)
+    -- Create mappings which use data from entry under cursor ~
+    -- Set focused directory as current working directory
+    local set_cwd = function()
+      local path = (MiniFiles.get_fs_entry() or {}).path
+      if path == nil then
+        return vim.notify("Cursor is not on valid entry")
+      end
+      vim.fn.chdir(vim.fs.dirname(path))
     end
+
+    -- Yank in register full path of entry under cursor
+    local yank_path = function()
+      local path = (MiniFiles.get_fs_entry() or {}).path
+      if path == nil then
+        return vim.notify("Cursor is not on valid entry")
+      end
+      vim.fn.setreg(vim.v.register, path)
+    end
+
     vim.api.nvim_create_autocmd("User", {
       pattern = "MiniFilesBufferCreate",
       callback = function(args)
-        vim.keymap.set("n", "g~", files_set_cwd, { buffer = args.data.buf_id })
+        local b = args.data.buf_id
+        vim.keymap.set("n", "g~", set_cwd, { buffer = b, desc = "Set cwd" })
+        vim.keymap.set("n", "gy", yank_path, { buffer = b, desc = "Yank path" })
       end,
     })
 
+    -- Create mapping to toggle file explorer
     local minifiles_toggle = function()
       if not MiniFiles.close() then
         local ok, res = pcall(function()
