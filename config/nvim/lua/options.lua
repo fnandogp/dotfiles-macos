bdd59e66accd0e3fc4b8e2fce96172a2c268ae19
Fernando Pinheiro <fernando-dotcollective@Fernandos-MacBook-Pro-DotCollective.local>
2025-03-13 10:17:08 +1100

    feat: dotfiles update
    Removed unused configurations and updated package manager settings.
    
    This commit removes several outdated configurations from the Brewfile, aliasrc,
    nvim config, and removes the zk note-taking tool. It also updates the aliasrc
    and zshrc files to include pnpm settings, ensuring the package manager is
    correctly configured in the environment.  The Brewfile has been cleaned of
    unused taps, brews, casks, and mas items.

--- config/nvim/lua/options.lua
+++ config/nvim/lua/options.lua
@@ -7,17 +7,17 @@ vim.opt.expandtab = true -- Use spaces instead of tabs
 vim.opt.shiftwidth = 2 -- Size of an indent
 vim.opt.list = true -- Show some invisible characters (tabs...
 
-vim.opt.pumblend = 0 -- disable transparency for completion popup
-vim.opt.winblend = 0 -- disable transparency for documentation popup
+vim.opt.pumblend = 0 -- Disable transparency for completion popup
+vim.opt.winblend = 0 -- Disable transparency for documentation popup
 -- Fix markdown indentation settings
 vim.g.markdown_recommended_style = 0
 
---folding options
+-- Folding options
 vim.opt.foldexpr = "v:lua.vim.treesitter.foldexpr()" -- fold based on treesitter
 vim.opt.foldmethod = "expr" -- fold based on treesitter
 vim.opt.foldlevel = 99 -- start with all folds open
 vim.opt.foldlevelstart = 99 -- start with all folds open
 
--- views can only be fully collapsed with the global statusline
+-- Views can only be fully collapsed with the global statusline
 vim.opt.laststatus = 3
 vim.opt.conceallevel = 1
--- config/nvim/lua/plugins/note-taking.lua
+++ config/nvim/lua/plugins/note-taking.lua
@@ -8,15 +8,39 @@ return {
   },
   opts = {
     workspaces = {
-      {
-        name = "personal",
-        path = "~/Documents/Vaults/Personal/",
-      },
-      {
-        name = "work",
-        path = "~/Documents/Vaults/Work/",
-      },
+      { name = "work", path = "~/Documents/Vaults/Work/" },
     },
+    daily_notes = {
+      -- Optional, if you keep daily notes in a separate directory.
+      folder = "dailies",
+      -- Optional, if you want to change the date format for the ID of daily notes.
+      date_format = "%Y-%m-%d",
+      -- Optional, if you want to change the date format of the default alias of daily notes.
+      alias_format = "%B %-d, %Y",
+      -- Optional, default tags to add to each new daily note created.
+      default_tags = { "daily-notes" },
+      -- Optional, if you want to automatically insert a template from your template directory like 'daily.md'
+      template = nil,
+    },
+    -- Optional, customize how note IDs are generated given an optional title.
+    ---@param title string|?
+    ---@return string
+    note_id_func = function(title)
+      -- Create note IDs in a Zettelkasten format with a timestamp and a suffix.
+      -- In this case a note with the title 'My new note' will be given an ID that looks
+      -- like '1657296016-my-new-note', and therefore the file name '1657296016-my-new-note.md'
+      local suffix = ""
+      if title ~= nil then
+        -- If title is given, transform it into valid file name.
+        suffix = title:gsub(" ", "-"):gsub("[^A-Za-z0-9-]", ""):lower()
+      else
+        -- If title is nil, just add 4 random uppercase letters to the suffix.
+        for _ = 1, 4 do
+          suffix = suffix .. string.char(math.random(65, 90))
+        end
+      end
+      return tostring(os.time()) .. "-" .. suffix
+    end,
     mappings = {
       -- Overrides the 'gf' mapping to work on markdown/wiki links within your vault.
       ["gf"] = {
@@ -42,8 +66,11 @@ return {
     },
   },
   keys = {
-    { "<Leader>ns", "<cmd>ObsidianSearch<CR>", desc = "Obsidian search" },
-    { "<Leader>nl", "<cmd>ObsidianLinks<CR>", desc = "Obsidian links" },
-    { "<Leader>nb", "<cmd>ObsidianBackLinks<CR>", desc = "Obsidian back links" },
+    { "<Leader>nn", "<cmd>ObsidianNew<CR>", desc = "Create an Obsidian note" },
+    { "<Leader>nf", "<cmd>ObsidianQuickSwitch<CR>", desc = "Find an Obsidian note" },
+    { "<Leader>ns", "<cmd>ObsidianSearch<CR>", desc = "Search for an Obsidian note" },
+    { "<Leader>nl", "<cmd>ObsidianLinks<CR>", desc = "List Obsidian links" },
+    { "<Leader>nb", "<cmd>ObsidianBackLinks<CR>", desc = "List Obsidian back links" },
+    { "<Leader>nt", "<cmd>ObsidianToday<CR>", desc = "Create a new daily note in Obsidian" },
   },
 }
