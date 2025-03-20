d281792ebe59ade084267adcdc3f609c742e7aba
Fernando Pinheiro <fnandogp@gmail.com>
2025-03-21 09:15:21 +1100

    feat(plugins): add flash.nvim for quick navigation
    This commit introduces flash.nvim, a plugin that enables fast and intuitive navigation within Neovim. It includes default keymappings for jumping, treesitter search, and toggling flash search.
    
    Signed-off-by: Fernando Pinheiro <fnandogp@gmail.com>

--- config/nvim/lua/plugins/coding.lua
+++ config/nvim/lua/plugins/coding.lua
@@ -5,6 +5,19 @@ return {
   { "echasnovski/mini.bufremove", version = false, opts = {} },
   { "echasnovski/mini.move", version = false, opts = {} },
   { "echasnovski/mini.ai", version = false, opts = {} },
+  {
+    "folke/flash.nvim",
+    event = "VeryLazy",
+    ---@type Flash.Config
+    opts = {},
+    keys = {
+      { "<enter>", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+      { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+      { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
+      { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+      { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
+    },
+  },
   { "sindrets/diffview.nvim", opts = {}, keys = {
     { "q", "<cmd>DiffviewClose<CR>", desc = "Close Diffview", mode = "n", ft = "DiffviewFiles" },
   } },
